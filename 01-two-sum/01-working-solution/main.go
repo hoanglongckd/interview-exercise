65b05a053744e1c2e5d86674ce1595d632575934
Long Hoang Tran <Long.Hoang.Tran@mgm-tp.com>
2021-09-03 15:47:23 +0700

    refactor the code to make it clearer

--- 01-two-sum/01-working-solution/main.go
+++ 01-two-sum/01-working-solution/main.go
@@ -1,12 +1,12 @@
 package _1_working_solution
 
 func TwoSum(nums []int, target int) []int {
-	if len(nums) > 1 {
-		for i := 0; i < len(nums)-1; i++ {
-			for j := i + 1; j < len(nums); j++ {
-				if nums[i]+nums[j] == target {
-					return []int{i, j}
-				}
+	for i := 0; i < len(nums)-1; i++ {
+		numberToFind := target - nums[i]
+
+		for j := i + 1; j < len(nums); j++ {
+			if nums[j] == numberToFind {
+				return []int{i, j}
 			}
 		}
 	}
